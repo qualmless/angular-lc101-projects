93dd98d455db9f16b9b52059d2435507b5320e0f
qualmless <terasmash@gmail.com>
2019-12-16 20:51:51 -0600

    just so many changes

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'All the most wondeful photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
+  image2 = 'https://i.pinimg.com/564x/df/a8/06/dfa80636b5349f3720d528e4262ced1d.jpg';
   image3 = '';
 
   constructor() { }
